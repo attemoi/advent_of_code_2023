2d819e0c27189ed37e14d6a061e3f02df761eb6e
Atte Moisio <attemoisio@attemoisio.fi>
2024-01-07 19:08:22 +0200

    Determine hand types for day 7

--- 07-camel-cards/src/Main.purs
+++ 07-camel-cards/src/Main.purs
@@ -2,9 +2,11 @@ module Main where
 
 import Prelude
 import Control.Alt ((<|>))
-import Data.Array (mapMaybe)
+import Data.Array (foldl, mapMaybe)
 import Data.Either (hush)
+import Data.Foldable (any, length)
 import Data.Generic.Rep (class Generic)
+import Data.Map (Map, empty, filter, insertWith, values)
 import Data.Maybe (Maybe)
 import Data.Show.Generic (genericShow)
 import Effect (Effect)
@@ -55,12 +57,17 @@ derive instance eqHandType :: Eq HandType
 
 derive instance ordHandType :: Ord HandType
 
+derive instance genericHandType :: Generic HandType _
+
+instance showHandType :: Show HandType where
+  show = genericShow
+
 main :: Effect Unit
 main = do
   inputLines <- readLines "07-camel-cards/input.txt"
   let
     hands = mapMaybe parseHand inputLines
-  log $ show $ hands
+  log $ show $ map handType $ map _.cards $ hands
 
 parseHand :: String -> Maybe Hand
 parseHand input = hush $ runParser input handParser
@@ -87,3 +94,19 @@ cardParser =
     <|> (string "4" $> Four)
     <|> (string "3" $> Three)
     <|> (string "2" $> Two)
+
+handType :: Array Card -> HandType
+handType cards
+  | nCards 5 cards = FiveOfKind
+  | nCards 4 cards = FourOfKind
+  | nCards 3 cards && nCards 2 cards = FullHouse
+  | nCards 3 cards = ThreeOfKind
+  | length (filter (\count -> count == 2) (cardCounts cards)) == 2 = TwoPairs
+  | nCards 2 cards = OnePair
+  | otherwise = HighCard
+
+nCards :: Int -> Array Card -> Boolean
+nCards n cards = any (\count -> count == n) (values $ cardCounts cards)
+
+cardCounts :: Array Card -> Map Card Int
+cardCounts cards = foldl (\map card -> insertWith (+) card 1 map) empty cards
