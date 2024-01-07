800455a4607a5cd6db0e7ab7641791c5e50a4ea8
Atte Moisio <attemoisio@attemoisio.fi>
2024-01-07 22:33:15 +0200

    Solve day 7, part 1

--- 07-camel-cards/src/Main.purs
+++ 07-camel-cards/src/Main.purs
@@ -2,23 +2,48 @@ module Main where
 
 import Prelude
 import Control.Alt ((<|>))
-import Data.Array (foldl, mapMaybe)
+import Data.Array (mapMaybe, sort)
 import Data.Either (hush)
-import Data.Foldable (any, length)
+import Data.Foldable (any, foldl, length)
 import Data.Generic.Rep (class Generic)
+import Data.List (List(..), (:))
 import Data.Map (Map, empty, filter, insertWith, values)
 import Data.Maybe (Maybe)
 import Data.Show.Generic (genericShow)
+import Data.Tuple (snd)
+import Data.Tuple.Nested ((/\))
 import Effect (Effect)
 import Effect.Console (log)
 import Parsing (Parser, runParser)
-import Parsing.Combinators.Array (many)
+import Parsing.Combinators (many)
 import Parsing.String (string)
 import Parsing.String.Basic (intDecimal, skipSpaces)
 import Utils (readLines)
 
-type Hand
-  = { cards :: Array Card, bid :: Int }
+newtype Hand
+  = Hand { cards :: List Card, bid :: Int }
+
+handCards :: Hand -> List Card
+handCards (Hand h) = _.cards h
+
+handBid :: Hand -> Int
+handBid (Hand h) = _.bid h
+
+derive newtype instance eqHand :: Eq Hand
+
+instance ordHand :: Ord Hand where
+  compare a b
+    | (handType $ handCards a) == (handType $ handCards b) = compareHighCards (handCards a) (handCards b)
+    | otherwise = compare (handType $ handCards a) (handType $ handCards b)
+
+compareHighCards :: List Card -> List Card -> Ordering
+compareHighCards Nil _ = LT
+
+compareHighCards _ Nil = GT
+
+compareHighCards (a : as) (b : bs)
+  | a == b = compareHighCards as bs
+  | otherwise = compare a b
 
 data Card
   = Two
@@ -66,18 +91,27 @@ main :: Effect Unit
 main = do
   inputLines <- readLines "07-camel-cards/input.txt"
   let
-    hands = mapMaybe parseHand inputLines
-  log $ show $ map handType $ map _.cards $ hands
+    hands = sort $ mapMaybe parseHand inputLines
+
+    totalWinnings =
+      snd
+        $ foldl
+            ( \(i /\ acc) hand ->
+                (i + 1) /\ (acc + (handBid hand) * (i + 1))
+            )
+            (0 /\ 0)
+            hands
+  log $ show $ "Part1 (total winnings): " <> (show $ totalWinnings)
 
 parseHand :: String -> Maybe Hand
 parseHand input = hush $ runParser input handParser
 
 handParser :: Parser String Hand
 handParser = do
-  cards <- many cardParser
+  cards' <- many cardParser
   skipSpaces
-  bid <- intDecimal
-  pure { cards: cards, bid: bid }
+  bid' <- intDecimal
+  pure (Hand { cards: cards', bid: bid' })
 
 cardParser :: Parser String Card
 cardParser =
@@ -95,7 +129,7 @@ cardParser =
     <|> (string "3" $> Three)
     <|> (string "2" $> Two)
 
-handType :: Array Card -> HandType
+handType :: List Card -> HandType
 handType cards
   | nCards 5 cards = FiveOfKind
   | nCards 4 cards = FourOfKind
@@ -105,8 +139,8 @@ handType cards
   | nCards 2 cards = OnePair
   | otherwise = HighCard
 
-nCards :: Int -> Array Card -> Boolean
+nCards :: Int -> List Card -> Boolean
 nCards n cards = any (\count -> count == n) (values $ cardCounts cards)
 
-cardCounts :: Array Card -> Map Card Int
+cardCounts :: List Card -> Map Card Int
 cardCounts cards = foldl (\map card -> insertWith (+) card 1 map) empty cards
