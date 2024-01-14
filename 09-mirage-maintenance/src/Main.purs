eb612e209675a799d50ab4b5a36526c0800124c1
Atte Moisio <attemoisio@attemoisio.fi>
2024-01-14 13:05:30 +0200

    Solve day 9

--- 09-mirage-maintenance/src/Main.purs
+++ 09-mirage-maintenance/src/Main.purs
@@ -1,9 +1,50 @@
 module Main where
 
 import Prelude
+import Data.Array as Array
+import Data.Either (hush)
+import Data.Foldable (all, sum)
+import Data.List (List(..), head, last, zipWith, (:))
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple, fst, snd)
+import Data.Tuple.Nested ((/\))
 import Effect (Effect)
 import Effect.Console (log)
+import Parsing (Parser, runParser)
+import Parsing.Combinators (sepBy)
+import Parsing.String.Basic (intDecimal, skipSpaces)
+import Utils (readLines)
 
 main :: Effect Unit
 main = do
-  log "Day 9 TODO"
+  inputLines <- readLines "09-mirage-maintenance/input.txt"
+  let
+    valueHistories = Array.mapMaybe parseValueHistory inputLines
+
+    extrapolations = Array.mapMaybe extrapolate valueHistories
+  log $ show $ "Part 1, sum of forwards extrapolated values: "
+    <> (show $ sum $ map snd $ extrapolations)
+  log $ show $ "Part 2, sum of backwards extrapolated values: "
+    <> (show $ sum $ map fst $ extrapolations)
+
+extrapolate :: List Int -> Maybe (Tuple Int Int)
+extrapolate Nil = Nothing
+
+extrapolate list
+  | all (_ == 0) list = Just (0 /\ 0)
+  | otherwise = do
+    last <- last list
+    head <- head list
+    (b /\ f) <- extrapolate (differences list)
+    pure ((head - b) /\ (last + f))
+
+differences :: List Int -> List Int
+differences Nil = Nil
+
+differences (x : xs) = zipWith (-) xs (x : xs)
+
+parseValueHistory :: String -> Maybe (List Int)
+parseValueHistory input = hush $ runParser input valueHistory
+
+valueHistory :: Parser String (List Int)
+valueHistory = intDecimal `sepBy` skipSpaces
--- 09-mirage-maintenance/test/Test/Main.purs
+++ 09-mirage-maintenance/test/Test/Main.purs
@@ -1,10 +1,30 @@
 module Test.Main where
 
 import Prelude
-
+import Data.List (List(..), (:))
+import Data.Maybe (Maybe(..))
+import Data.Tuple.Nested ((/\))
 import Effect (Effect)
-import Effect.Class.Console (log)
+import Effect.Aff (launchAff_)
+import Main (differences, extrapolate)
+import Test.Spec (describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
-main = do
-  log "You should add some tests."
+main =
+  launchAff_
+    $ runSpec [ consoleReporter ] do
+        describe "Main" do
+          it "should generate list of differences" do
+            let
+              result = differences (1 : 2 : 4 : Nil)
+
+              result2 = differences (1 : Nil)
+            result `shouldEqual` (1 : 2 : Nil)
+            result2 `shouldEqual` Nil
+          it "Should extrapolate" do
+            let
+              result = extrapolate (10 : 13 : 16 : 21 : 30 : 45 : Nil)
+            result `shouldEqual` (Just (5 /\ 68))
